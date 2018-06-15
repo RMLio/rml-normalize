0aa0ae729b438f0860ea9928b7bfc1c6e15f77a9
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2018-06-15 10:26:58 +0200

    add cli

---
+++ bin/cli.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+/**
+ * author: Pieter Heyvaert (pheyvaer.heyvaert@ugent.be)
+ * Ghent University - imec - IDLab
+ */
+
+const writeNormalizeRml = require('../index').writeNormalizeRml;
+const fs                = require('fs');
+const path              = require('path');
+const N3                = require('n3');
+
+let parser = new N3.Parser();
+let store = new N3.Store();
+
+let inputData = fs.readFileSync(path.resolve(process.cwd(), process.argv[2]), 'utf8');
+
+parser.parse(inputData,
+  function (error, triple, prefixes) {
+    if (triple)
+      store.addTriple(triple);
+    else {
+      let writer = N3.Writer({
+        prefixes: prefixes
+      });
+      writeNormalizeRml(store, writer);
+      writer.end(function (error, result) {
+        console.log(result);
+      });
+    }
+  });
